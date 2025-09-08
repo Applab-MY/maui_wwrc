365c33821c7b8f8709ab0bf214c671feafeaa890
rashid <muhdrashid@applab.com.my>
2025-09-08 12:57:28 +0800

    - rest api folder

---
+++ Content/RestApi/IRestService.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.ObjectModel;
+using wwrc_maui.Content.Model.Auth;
+using wwrc_maui.Content.Model.Common;
+using static wwrc_maui.Content.Model.Auth.LoginModel;
+using static wwrc_maui.Content.Model.CountryModel;
+using static wwrc_maui.Content.Model.CurrencyModel;
+using static wwrc_maui.Content.Model.CustomerAgingModel;
+using static wwrc_maui.Content.Model.CustomerVisitModel;
+using static wwrc_maui.Content.Model.DashboardModel;
+using static wwrc_maui.Content.Model.EmpHandbookModel;
+using static wwrc_maui.Content.Model.MediaModel;
+using static wwrc_maui.Content.Model.NewsModel;
+using static wwrc_maui.Content.Model.PasswordModel;
+using static wwrc_maui.Content.Model.POModel;
+using static wwrc_maui.Content.Model.ProductModel;
+using static wwrc_maui.Content.Model.SalesModel;
+using static wwrc_maui.Content.Model.SOModel;
+using static wwrc_maui.Content.Model.StaffModel;
+using static wwrc_maui.Content.Model.StockModel;
+
+namespace wwrc_maui.Content.RestApi
+{
+    public interface IRestService
+    {
+        Task<RequestResult<ObservableCollection<LoginModel>>> Login(API_LoginModel model);
+        Task<RequestResult<ObservableCollection<LoginModel>>> MicrosoftLogin(API_MicrosoftLoginModel model);
+        Task<RequestResult<ObservableCollection<StaffMainModel>>> Staff(API_StaffModel model);
+        Task<RequestResult<ObservableCollection<MediaMainModel>>> GetPhoto();
+        Task<RequestResult<ObservableCollection<NewsMainModel>>> GetNews();
+        Task<RequestResult<ObservableCollection<CountryMainModel>>> GetCountry();
+        Task<RequestResult<ObservableCollection<ProductMainModel>>> ProductCatalog(API_ProductCatalogModel model);
+        Task<RequestResult<ObservableCollection<EmployeeHandbookMainModel>>> GetEmployeeHandbook(API_EmployeeHandbook model);
+        Task<RequestResult<ObservableCollection<CustomerVisitMainModel>>> GetCustomerVisit(API_CustomerVisitModel model);
+        Task<RequestResult<CurrencyMainModel>> GetCurrency(API_Currency model);
+        Task<RequestResult<ObservableCollection<ResponseHeader>>> CreateCustomerVisit(API_CreateCustomerVisit model);
+        Task<RequestResult<ObservableCollection<ResponseHeader>>> UpdateCustomerVisit(API_UpdateCustomerVisit model);
+        Task<RequestResult<ObservableCollection<DashboardMainModel>>> GetDashBoard(API_DashBoard model);
+        Task<RequestResult<ObservableCollection<DashboardMainModel>>> GetDashBoardOffLine(API_DashBoard model, bool isrefresh = false);
+        Task<RequestResult<ObservableCollection<SalesMainModel>>> GetSalesResult(API_DashBoard model);
+        Task<RequestResult<ObservableCollection<StockAlertMainModel>>> GetStockAlert(API_StockAlert model);
+        Task<RequestResult<ObservableCollection<ChangePasswordModel>>> ChangePassword(API_ChangePasswordModel model);
+        Task<RequestResult<ObservableCollection<SalesOrderMainModel>>> GetSalesOrder(API_SalesOrder model);
+        Task<RequestResult<ObservableCollection<DOList>>> GetDOBySalesOrder(API_DObySO model);
+        Task<RequestResult<ObservableCollection<POMainModel>>> GetPurchase(API_PurchaseModel model);
+        Task<RequestResult<ObservableCollection<CustomerAgingMainModel>>> GetCustomerAging(API_CustomerAging model);
+        Task<RequestResult<ObservableCollection<AgingDetailMainModel>>> GetCustomerAgingDetail(API_CustomerAging model);
+        Task<bool> ProfileUpdate(MultipartFormDataContent content);
+        Task<bool> UpdateNews(MultipartFormDataContent content);
+        Task<bool> UpdateFCMToken(string platform, string imei, string token, string userId);
+        Task<bool> ForgotPassword(MultipartFormDataContent content);
+        Task<bool> ReadPhoto(MultipartFormDataContent content);
+        Task<bool> ReadVideo(MultipartFormDataContent content);
+        Task<bool> ReadEmployeeHandbook(MultipartFormDataContent content);
+        Task<bool> ReadProductCatalog(MultipartFormDataContent content);
+    }
+}
---
+++ Content/RestApi/RestService.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections.ObjectModel;
+using wwrc_maui.Content.Model;
+using wwrc_maui.Content.Model.Auth;
+using wwrc_maui.Content.Model.Common;
+using static wwrc_maui.Content.Model.Auth.LoginModel;
+using static wwrc_maui.Content.Model.CurrencyModel;
+using static wwrc_maui.Content.Model.CustomerAgingModel;
+using static wwrc_maui.Content.Model.CustomerVisitModel;
+using static wwrc_maui.Content.Model.DashboardModel;
+using static wwrc_maui.Content.Model.EmpHandbookModel;
+using static wwrc_maui.Content.Model.PasswordModel;
+using static wwrc_maui.Content.Model.POModel;
+using static wwrc_maui.Content.Model.ProductModel;
+using static wwrc_maui.Content.Model.SOModel;
+using static wwrc_maui.Content.Model.StaffModel;
+using static wwrc_maui.Content.Model.StockModel;
+
+namespace wwrc_maui.Content.RestApi
+{
+    public class RestService : IRestService
+    {
+        Task<RequestResult<ObservableCollection<ChangePasswordModel>>> IRestService.ChangePassword(API_ChangePasswordModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<RequestResult<ObservableCollection<ResponseHeader>>> IRestService.CreateCustomerVisit(API_CreateCustomerVisit model)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> IRestService.ForgotPassword(MultipartFormDataContent content)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<RequestResult<ObservableCollection<CountryModel.CountryMainModel>>> IRestService.GetCountry()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<RequestResult<CurrencyMainModel>> IRestService.GetCurrency(API_Currency model)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<RequestResult<ObservableCollection<CustomerAgingMainModel>>> IRestService.GetCustomerAging(API_CustomerAging model)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<RequestResult<ObservableCollection<AgingDetailMainModel>>> IRestService.GetCustomerAgingDetail(API_CustomerAging model)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<RequestResult<ObservableCollection<CustomerVisitMainModel>>> IRestService.GetCustomerVisit(API_CustomerVisitModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<RequestResult<ObservableCollection<DashboardMainModel>>> IRestService.GetDashBoard(API_DashBoard model)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<RequestResult<ObservableCollection<DashboardMainModel>>> IRestService.GetDashBoardOffLine(API_DashBoard model, bool isrefresh)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<RequestResult<ObservableCollection<DOList>>> IRestService.GetDOBySalesOrder(API_DObySO model)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<RequestResult<ObservableCollection<EmployeeHandbookMainModel>>> IRestService.GetEmployeeHandbook(API_EmployeeHandbook model)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<RequestResult<ObservableCollection<NewsModel.NewsMainModel>>> IRestService.GetNews()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<RequestResult<ObservableCollection<MediaModel.MediaMainModel>>> IRestService.GetPhoto()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<RequestResult<ObservableCollection<POMainModel>>> IRestService.GetPurchase(API_PurchaseModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<RequestResult<ObservableCollection<SalesOrderMainModel>>> IRestService.GetSalesOrder(API_SalesOrder model)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<RequestResult<ObservableCollection<SalesModel.SalesMainModel>>> IRestService.GetSalesResult(API_DashBoard model)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<RequestResult<ObservableCollection<StockAlertMainModel>>> IRestService.GetStockAlert(API_StockAlert model)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<RequestResult<ObservableCollection<LoginModel>>> IRestService.Login(API_LoginModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<RequestResult<ObservableCollection<LoginModel>>> IRestService.MicrosoftLogin(API_MicrosoftLoginModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<RequestResult<ObservableCollection<ProductModel.ProductMainModel>>> IRestService.ProductCatalog(API_ProductCatalogModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> IRestService.ProfileUpdate(MultipartFormDataContent content)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> IRestService.ReadEmployeeHandbook(MultipartFormDataContent content)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> IRestService.ReadPhoto(MultipartFormDataContent content)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> IRestService.ReadProductCatalog(MultipartFormDataContent content)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> IRestService.ReadVideo(MultipartFormDataContent content)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<RequestResult<ObservableCollection<StaffMainModel>>> IRestService.Staff(API_StaffModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<RequestResult<ObservableCollection<ResponseHeader>>> IRestService.UpdateCustomerVisit(API_UpdateCustomerVisit model)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> IRestService.UpdateFCMToken(string platform, string imei, string token, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> IRestService.UpdateNews(MultipartFormDataContent content)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

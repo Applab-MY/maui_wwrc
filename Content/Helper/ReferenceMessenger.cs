7b8599808162c69d12fdb5bb92164b922b023195
rashid <muhdrashid@applab.com.my>
2025-09-10 18:41:20 +0800

    - Helper :: reference messenger

---
+++ Content/Helper/ReferenceMessenger.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+
+namespace wwrc_maui.Content.Helper
+{
+    public class ReferenceMessenger
+    {
+        public class ClassObjectNotify(KeyClassObject value) : ValueChangedMessage<KeyClassObject>(value) { }
+        public class StringNotify(string value) : ValueChangedMessage<string>(value) { }
+        public class KeyValueNotify(KeyValue value) : ValueChangedMessage<KeyValue>(value) { }
+
+        public class KeyValue()
+        {
+            public string Key { get; set; } = "";
+            public string Value { get; set; } = "";
+            public string Description { get; set; } = "";
+        }
+
+        public class KeyClassObject()
+        {
+            public string Key { get; set; } = "";
+            public object? Value { get; set; } = null;
+            public string Description { get; set; } = "";
+        }
+    }
+}

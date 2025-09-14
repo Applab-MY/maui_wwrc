cb9e330e09db9690604f01186ae0a17253404865
rashid <muhdrashid@applab.com.my>
2025-09-14 23:12:00 +0800

    - Helper :: string null converter

---
+++ Content/Helper/StringNullConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace wwrc_maui.Content.Helper
+{
+    public class StringNullConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            var text = " - ";
+            if (value != null) text = value.ToString();
+            if (!string.IsNullOrEmpty(text))
+            {
+                if (text.Contains("NULL"))
+                    text = text.ToString().Replace("NULL", " - ");
+            }
+            return text;
+        }
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}

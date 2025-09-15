dd21e2f7c568046bbc036fe96b5fd8de4bf0ca14
rashid <muhdrashid@applab.com.my>
2025-09-15 15:08:16 +0800

    - Viewmodel :: staff

---
+++ Content/Viewmodels/Staff/CustomerVisitVm.cs
@@ -0,0 +1,12 @@
+﻿using wwrc_maui.Content.Viewmodels.Common;
+
+namespace wwrc_maui.Content.Viewmodels.Staff
+{
+    public class CustomerVisitVm : BaseViewModel
+    {
+        public CustomerVisitVm()
+        {
+            IsBusy = false;
+        }
+    }
+}
---
+++ Content/Viewmodels/Staff/StaffDirectoryVm.cs
@@ -0,0 +1,12 @@
+﻿using wwrc_maui.Content.Viewmodels.Common;
+
+namespace wwrc_maui.Content.Viewmodels.Staff
+{
+    public class StaffDirectoryVm : BaseViewModel
+    {
+        public StaffDirectoryVm()
+        {
+            IsBusy = false;
+        }
+    }
+}

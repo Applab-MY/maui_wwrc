137a89cddef100746b09bf2b1d5fea259a02062a
rashid <muhdrashid@applab.com.my>
2025-09-17 19:15:02 +0800

    - Platform :: android, ios

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -6,11 +6,10 @@ using RGPopup.Maui.Droid;
 
 namespace wwrc_maui
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop,
-        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode |
-        ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density,
-        ScreenOrientation = ScreenOrientation.Portrait | ScreenOrientation.Landscape,
-        WindowSoftInputMode = SoftInput.AdjustResize)]
+    [Activity(Label = "WWRC", Icon = "@drawable/launcher", Theme = "@style/Maui.SplashTheme", MainLauncher = true,
+        LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation |
+        ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density,
+        ScreenOrientation = ScreenOrientation.Portrait | ScreenOrientation.Landscape, WindowSoftInputMode = SoftInput.AdjustResize)]
     public class MainActivity : MauiAppCompatActivity
     {
         protected override void OnCreate(Bundle? bundle)

905c602aab12a6bcab531c31b06a18bb6269b685
rashid <muhdrashid@applab.com.my>
2025-09-19 19:10:07 +0800

    - Views :: login

--- Content/Views/Auth/Login.xaml.cs
+++ Content/Views/Auth/Login.xaml.cs
@@ -1,5 +1,7 @@
+using CommunityToolkit.Mvvm.Messaging;
 using wwrc_maui.Content.Model;
 using wwrc_maui.Content.Viewmodels.Auth;
+using static wwrc_maui.Content.Helper.ReferenceMessenger;
 
 namespace wwrc_maui.Content.Views.Auth;
 
@@ -11,7 +13,19 @@ public partial class Login : ContentPage
     {
         InitializeComponent();
         BindingContext = viewmodel;
-        var cek = AppDatabase.Instance.SqlConnection;
+
+        WeakReferenceMessenger.Default.Register<KeyValueNotify>(this, async (receiver, message) =>
+        {
+            if (message.Value != null && message.Value.Key.Equals("O365login"))
+            {
+                //2001:usercancel, 2003:success
+                if (message.Value.Value.Equals("2001"))
+                    await App.DisplayAlert("Cancel", "Login cancelled.", null, "Close");
+                else if (message.Value.Value.Equals("2003"))
+                    await App.DisplayAlert(message.Value.Value, "login success", null, "Okay");
+                viewmodel.IsBusy = false; viewmodel.IsRefreshing = false;
+            }
+        });
     }
 
     private async void OnForgotPwd_Tapped(object sender, TappedEventArgs e)

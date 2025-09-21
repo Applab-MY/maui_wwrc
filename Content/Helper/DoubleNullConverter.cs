fe16718fd1e00b62588239499d9420fe69160796
rashid <muhdrashid@applab.com.my>
2025-09-21 23:52:24 +0800

    - Helper :: double null converter

---
+++ Content/Helper/DoubleNullConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace wwrc_maui.Content.Helper
+{
+    public class DoubleNullConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            var _no = (double?)value;
+            return _no == 0.0 ? false : true;
+        }
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            var _no = (double?)value;
+            return _no == 0.0 ? true : false;
+        }
+    }
+}

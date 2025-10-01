d88b8e02df9892cf7a0f0ead0980730b6827bd16
rashid <muhdrashid@applab.com.my>
2025-10-01 19:18:04 +0800

    - Custom Controls :: borderless timepicker

---
+++ Content/CustomControls/Base/BorderlessTimePicker.cs
@@ -0,0 +1,13 @@
+﻿namespace wwrc_maui.Content.CustomControls.Base
+{
+    public class BorderlessTimePicker : TimePicker
+    {
+        public BorderlessTimePicker()
+        {
+            TextColor = Colors.Black;
+            HorizontalOptions = LayoutOptions.Fill;
+            FontSize = 14;
+            HeightRequest = 50;
+        }
+    }
+}
